85541464bfe8f3dab9c940e8fd9733c28d6b5d5f
RyanPCepada <200881150+grislicepada2@users.noreply.github.com>
2025-04-22 18:29:22 +0800

    added charset=utf8mb4 to display characters properly

--- config.php
+++ config.php
@@ -6,10 +6,13 @@ $username = 'root';
 $password =''; 
 
 try {
-    // $pdo = new PDO("mysql:host=$host; port=$port; dbname=$dbname", $username, $password);
-    $pdo = new PDO("mysql:host=$host;  dbname=$dbname", $username, $password);
+    // Include charset=utf8mb4 here
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Connection successful!";
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
+
 ?>
